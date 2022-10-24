3494178840cdb7229880d9e28f24aa4a4beccbc7
Kevin O'Neill <oneill.kevin.96@gmail.com>
2022-10-24 18:28:01 +0100

    Project in Docker

---
+++ src/main/kotlin/com/project/tasks/TasksApplication.kt
@@ -0,0 +1,12 @@
+package com.project.tasks
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TasksApplication
+
+fun main(args: Array<String>) {
+	runApplication<TasksApplication>(*args)
+	println("Running application...")
+}
